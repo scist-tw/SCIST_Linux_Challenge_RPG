e77cf6db7575b06406d4b48131da1f909fd977cd
Curious-Lucifer <curious.lucifer.domini@gmail.com>
2023-09-14 20:56:38 +0800

    Update build/push image

--- build_image.sh
+++ build_image.sh
@@ -4,11 +4,11 @@ version=2.0
 architecture=$(uname -m)
 
 if [[ $architecture == "aarch64" ]]; then
-    docker build --platform linux/arm64 -t curiouslucifer/scist_linux_challenge_rpg:$version-challenge_box ./Challenge_Box
-    docker build --platform linux/arm64 -t curiouslucifer/scist_linux_challenge_rpg:$version-web_service ./Web_Service
-    docker build --platform linux/arm64 -t curiouslucifer/scist_linux_challenge_rpg:$version-ssh_service ./SSH_Service
+    docker build --platform linux/arm64 -t curiouslucifer/scist_linux_challenge_rpg:$version-challenge_box-arm64 ./Challenge_Box
+    docker build --platform linux/arm64 -t curiouslucifer/scist_linux_challenge_rpg:$version-web_service-arm64 ./Web_Service
+    docker build --platform linux/arm64 -t curiouslucifer/scist_linux_challenge_rpg:$version-ssh_service-arm64 ./SSH_Service
 elif [[ $architecture == "x86_64" ]]; then
-    docker build --platform linux/amd64 -t curiouslucifer/scist_linux_challenge_rpg:$version-challenge_box ./Challenge_Box
-    docker build --platform linux/amd64 -t curiouslucifer/scist_linux_challenge_rpg:$version-web_service ./Web_Service
-    docker build --platform linux/amd64 -t curiouslucifer/scist_linux_challenge_rpg:$version-ssh_service ./SSH_Service
+    docker build --platform linux/amd64 -t curiouslucifer/scist_linux_challenge_rpg:$version-challenge_box-amd64 ./Challenge_Box
+    docker build --platform linux/amd64 -t curiouslucifer/scist_linux_challenge_rpg:$version-web_service-amd64 ./Web_Service
+    docker build --platform linux/amd64 -t curiouslucifer/scist_linux_challenge_rpg:$version-ssh_service-amd64 ./SSH_Service
 fi
--- push_image.sh
+++ push_image.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 version=2.0
+architecture=$(uname -m)
 
-docker push curiouslucifer/scist_linux_challenge_rpg:$version-challenge_box
-docker push curiouslucifer/scist_linux_challenge_rpg:$version-web_service
-docker push curiouslucifer/scist_linux_challenge_rpg:$version-ssh_service
+if [[ $architecture == "aarch64" ]]; then
+    docker push curiouslucifer/scist_linux_challenge_rpg:$version-challenge_box-arm64
+    docker push curiouslucifer/scist_linux_challenge_rpg:$version-web_service-arm64
+    docker push curiouslucifer/scist_linux_challenge_rpg:$version-ssh_service-arm64
+elif [[ $architecture == "x86_64" ]]; then
+    docker push curiouslucifer/scist_linux_challenge_rpg:$version-challenge_box-amd64
+    docker push curiouslucifer/scist_linux_challenge_rpg:$version-web_service-amd64
+    docker push curiouslucifer/scist_linux_challenge_rpg:$version-ssh_service-amd64
+fi
